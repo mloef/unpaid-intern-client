314f491d0963a87d96076b5ce2a125f32fc2f83e
msl <maxsloeffler@gmail.com>
2023-09-21 23:02:58 -0700

    working sms version

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const { spawn } = require('child_process');
+const WebSocket = require('ws');
+
+const ws = new WebSocket('ws://52.33.88.92/ws/');
+
+// Start a new shell process
+const shell = spawn('bash');
+
+// Handle shell outputs
+shell.stdout.on('data', (data) => {
+    sendToRenderer('ws-message', JSON.stringify({type: 'LLM_RESULT', body: data.toString()}));
+    ws.send(JSON.stringify({ type: 'LLM_RESULT', body: data.toString() }));
+});
+
+shell.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+});
+
+shell.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+});
+
+// Send commands to the shell
+shell.stdin.write('export OPENAI_API_KEY="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"\n');
+shell.stdin.write('source /Users/msl/src/scratch/.venv/bin/activate\n');
+shell.stdin.write('interpreter -y\n');
 
 let mainWindow;
 
 function createWindow() {
     mainWindow = new BrowserWindow({
-        width: 400,
-        height: 500,
+        width: 1200,
+        height: 1500,
         webPreferences: {
-            nodeIntegration: true
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true
         }
     });
 
+    mainWindow.webContents.openDevTools();
     mainWindow.loadFile('index.html');
 }
 
@@ -27,3 +56,29 @@ app.on('activate', function () {
         createWindow();
     }
 });
+
+ws.onopen = () => {
+    ws.send(JSON.stringify({ type: 'CLIENT_ID', id: '+12078319547' })); //TODO
+    sendToRenderer('ws-open', null);
+    console.log('WebSocket Client Connected');
+};
+
+ws.onmessage = (event) => {
+    sendToRenderer('ws-message', event.data);
+    const data = JSON.parse(event.data);
+    if (data.type === 'SEND_MESSAGE') {
+        shell.stdin.write(data.body + '\n');
+    }
+};
+
+ws.onerror = (error) => {
+    sendToRenderer('ws-error', error);
+};
+
+ws.onclose = (event) => {
+    sendToRenderer('ws-close', { code: event.code, reason: event.reason });
+};
+
+function sendToRenderer(type, data) {
+    mainWindow.webContents.send(type, data);
+}
--- renderer.js
+++ renderer.js
@@ -1,30 +1,28 @@
-const ws = new WebSocket('ws://52.33.88.92/ws/');
-
 const chatDiv = document.getElementById('chat');
 
-ws.onopen = () => {
+// Listen for events from main process
+window.electronAPI.receive('ws-open', () => {
     chatDiv.innerHTML += `<p>Connected to the server<p>`;
-    ws.send(JSON.stringify({ type: 'CLIENT_ID', id: '+12078319547' }));
-};
+});
 
-ws.onmessage = (event) => {
-    const data = JSON.parse(event.data);
+window.electronAPI.receive('ws-message', (data) => {
+    data = JSON.parse(data);
     if (data.type === 'YOUR_ID') {
         chatDiv.innerHTML += `<p>Your client ID: ${data.id}</p>`;
     } else if (data.type === 'SEND_MESSAGE') {
         chatDiv.innerHTML += `<p>Message for LLM: ${data.body}</p>`;
-        const reversedMessage = data.body.split('').reverse().join('');
-        ws.send(JSON.stringify({type: "SEND_MESSAGE", body: reversedMessage}));
-        chatDiv.innerHTML += `<p>Message from LLM: ${reversedMessage}</p>`;
+    } else if (data.type === 'LLM_RESULT') {
+        console.log(data.body.toString())
+        chatDiv.innerHTML += `<p>Message from LLM: ${data.body.toString()}</p>`;
     } else {
-        chatDiv.innerHTML += `<p>Got unknown message: ${event.data}</p>`;
+        chatDiv.innerHTML += `<p>Got unknown message: ${JSON.stringify(data)}</p>`;
     }
-};
+});
 
-ws.onerror = (error) => {
+window.electronAPI.receive('ws-error', (error) => {
     chatDiv.innerHTML += `WebSocket Error: ` + error;
-};
+});
 
-ws.onclose = (event) => {
-    chatDiv.innerHTML += 'WebSocket closed with code:' + event.code + ' and reason:' + event.reason;
-};
+window.electronAPI.receive('ws-close', (data) => {
+    chatDiv.innerHTML += 'WebSocket closed with code:' + data.code + ' and reason:' + data.reason;
+});
