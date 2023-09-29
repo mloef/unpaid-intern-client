d32b879658b0d258aedf42454c845678acf42821
msl <maxsloeffler@gmail.com>
2023-09-29 11:54:01 -0700

    ugh

--- run_interpreter.py
+++ run_interpreter.py
@@ -1,5 +1,7 @@
 import multiprocessing
+import threading
 import interpreter
+import queue  # to handle the Empty exception from Queue.get_nowait()
 
 switch = {
     "message": lambda data: print(data),
@@ -11,67 +13,94 @@ switch = {
     "end_of_execution": lambda data: None
 }
 
-def process(input_text, result_queue):
-    buffer = {}
-    result = interpreter.chat(input_text, display=False, stream=True)
+def worker_process(input_queue, result_queue):
+    while True:
+        input_text = input_queue.get()
     
-    for entry in result:
-        if len(entry) != 1:
-            raise AssertionError(f"Expected entry length to be 1, but got {entry}")
+        buffer = {}
+        result = interpreter.chat(input_text, display=False, stream=True)
+        
+        for entry in result:
+            if len(entry) != 1:
+                raise AssertionError(f"Expected entry length to be 1, but got {entry}")
 
-        k, v = list(entry.items())[0]
-        if not buffer:
-            buffer[k] = v
-        elif buffer.get(k) and type(v) == str:
-            buffer[k] += v
-            if buffer[k][-1] == '\n':
+            k, v = list(entry.items())[0]
+            if not buffer:
+                buffer[k] = v
+            elif buffer.get(k) and type(v) == str:
+                buffer[k] += v
+                if buffer[k][-1] == '\n':
+                    if len(buffer) != 1:
+                        raise AssertionError(f"Expected buffer length to be 1, but got {buffer}")
+                    
+                    mode = k
+                    data = buffer[mode] + '** **'  # hack to preserve trailing \n in discord. doubles newline, though? why
+
+                    result_queue.put((mode, data))
+                    buffer = {}
+            else:
                 if len(buffer) != 1:
                     raise AssertionError(f"Expected buffer length to be 1, but got {buffer}")
                 
-                mode = k
-                data = buffer[mode] + '** **'  # hack to preserve trailing \n in discord. doubles newline, though? why
+                message = list(buffer.items())[0]
+                mode, data = message
 
                 result_queue.put((mode, data))
-                buffer = {}
-        else:
-            if len(buffer) != 1:
-                raise AssertionError(f"Expected buffer length to be 1, but got {buffer}")
-            
+                buffer = {k: v}
+
+        if buffer:
             message = list(buffer.items())[0]
             mode, data = message
-
             result_queue.put((mode, data))
-            buffer = {k: v}
 
-    if buffer:
-        message = list(buffer.items())[0]
-        mode, data = message
-        result_queue.put((mode, data))
-
-print('Welcome to unpaid intern! Send "reset" to reset the conversation.')
-print('%%END_OF_RESPONSE%%')
-
-current_process = None
-result_queue = multiprocessing.Queue()
-
-while True:
-    input_text = input()
-
-    if input_text == "reset":
-        if current_process and current_process.is_alive():
-            current_process.terminate()
-            current_process.join()  # wait for the process to truly terminate
-        interpreter.reset()
-        print("The intern forgot everything :)")
-        print('%%END_OF_RESPONSE%%')
-    else:
-        # Starting the process
-        current_process = multiprocessing.Process(target=process, args=(input_text, result_queue))
-        current_process.start()
-
-        # Wait for results and process them
-        while current_process.is_alive() or not result_queue.empty():
-            mode, data = result_queue.get()
+def result_listener(result_queue):
+    while True:
+        try:
+            mode, data = result_queue.get_nowait()  # non-blocking get
+            print('got data:', mode, data)
             switch.get(mode, lambda data: print("ERROR: could not parse", data))(data)
-        
-        print('%%END_OF_RESPONSE%%')
+            if mode == "end_of_execution":
+                print('%%END_OF_RESPONSE%%')
+        except queue.Empty:
+            pass
+
+def main():
+    print('Welcome to unpaid intern! Send "reset" to reset the conversation.')
+    print('%%END_OF_RESPONSE%%')
+
+    current_process = None
+    result_queue = multiprocessing.Queue()
+    input_queue = multiprocessing.Queue()
+
+    # Start the process for the first time
+    current_process = multiprocessing.Process(target=worker_process, args=(input_queue, result_queue))
+    current_process.start()
+
+    # Start the thread listening to results
+    threading.Thread(target=result_listener, args=(result_queue,), daemon=True).start()
+
+    while True:
+        input_text = input()
+
+        if input_text == "reset":
+            if current_process and current_process.is_alive():
+                current_process.terminate()
+                current_process.join()
+            
+            # Start a new process after termination
+            current_process = multiprocessing.Process(target=worker_process, args=(input_queue, result_queue))
+            current_process.start()
+            
+            print("The intern forgot everything :)")
+            print('%%END_OF_RESPONSE%%')
+        else:
+            input_queue.put(input_text)
+            # Wait for results and process them
+            while current_process.is_alive() or not result_queue.empty():
+                mode, data = result_queue.get()
+                switch.get(mode, lambda data: print("ERROR: could not parse", data))(data)
+            
+            print('%%END_OF_RESPONSE%%')
+
+if __name__ == '__main__':
+    main()
