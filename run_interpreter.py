bac783aa91fd9bde360822272f339101e9f39f3d
msl <maxsloeffler@gmail.com>
2023-09-29 12:18:37 -0700

    making progress

--- run_interpreter.py
+++ run_interpreter.py
@@ -2,6 +2,7 @@ import multiprocessing
 import threading
 import interpreter
 import queue  # to handle the Empty exception from Queue.get_nowait()
+import os
 
 switch = {
     "message": lambda data: print(data),
@@ -10,12 +11,15 @@ switch = {
     "language": lambda data: print("`Running " + data + ":`"),
     "active_line": lambda data: None,
     "executing": lambda data: None,
-    "end_of_execution": lambda data: None
+    "end_of_execution": lambda data: None,
+    "end_of_response": lambda data: print("%%END_OF_RESPONSE%%"),
 }
 
 def worker_process(input_queue, result_queue):
+    print('starting worker process')
     while True:
         input_text = input_queue.get()
+        print('got input_text:', input_text)
     
         buffer = {}
         result = interpreter.chat(input_text, display=False, stream=True)
@@ -53,14 +57,13 @@ def worker_process(input_queue, result_queue):
             mode, data = message
             result_queue.put((mode, data))
 
+        result_queue.put(("end_of_response", ""))
+
 def result_listener(result_queue):
     while True:
         try:
             mode, data = result_queue.get_nowait()  # non-blocking get
-            print('got data:', mode, data)
             switch.get(mode, lambda data: print("ERROR: could not parse", data))(data)
-            if mode == "end_of_execution":
-                print('%%END_OF_RESPONSE%%')
         except queue.Empty:
             pass
 
@@ -72,20 +75,21 @@ def main():
     result_queue = multiprocessing.Queue()
     input_queue = multiprocessing.Queue()
 
-    # Start the process for the first time
+    # Start the worker process for the first time
     current_process = multiprocessing.Process(target=worker_process, args=(input_queue, result_queue))
     current_process.start()
 
-    # Start the thread listening to results
-    threading.Thread(target=result_listener, args=(result_queue,), daemon=True).start()
+    # Start the process listening to results
+    multiprocessing.Process(target=result_listener, args=(result_queue,), daemon=True).start()
 
     while True:
         input_text = input()
-
+        print(input_queue.empty())
         if input_text == "reset":
             if current_process and current_process.is_alive():
                 current_process.terminate()
                 current_process.join()
+                print("The intern was fired :(", current_process.is_alive())
             
             # Start a new process after termination
             current_process = multiprocessing.Process(target=worker_process, args=(input_queue, result_queue))
@@ -94,13 +98,9 @@ def main():
             print("The intern forgot everything :)")
             print('%%END_OF_RESPONSE%%')
         else:
+            print('input:', input_text)
+            print('current_process.is_alive():', current_process.is_alive())
             input_queue.put(input_text)
-            # Wait for results and process them
-            while current_process.is_alive() or not result_queue.empty():
-                mode, data = result_queue.get()
-                switch.get(mode, lambda data: print("ERROR: could not parse", data))(data)
-            
-            print('%%END_OF_RESPONSE%%')
-
+        
 if __name__ == '__main__':
     main()
