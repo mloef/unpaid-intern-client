add6979db023a278b6b8c21058fafc635b9d2644
msl <maxsloeffler@gmail.com>
2023-09-30 11:43:41 -0700

    alright fuck this, we're eliminating multiprocessing

--- run_interpreter.py
+++ run_interpreter.py
@@ -2,6 +2,7 @@ import multiprocessing
 import queue  # to handle the Empty exception from Queue.get_nowait()
 import threading
 from typing import Callable, Dict, Tuple
+import time
 
 switch: Dict[str, Callable[[str], None]] = {
     "message": lambda data: print(data),
@@ -14,13 +15,18 @@ switch: Dict[str, Callable[[str], None]] = {
     "end_of_response": lambda data: print("%%END_OF_RESPONSE%%"),
 }
 
-def worker_process(input_queue: queue.Queue[str], result_queue: queue.Queue[Tuple[str, str]]) -> None:
+def worker_process(input_queue: queue.Queue[str]) -> None:
+    print('starting worker process')
     import interpreter #import starts the interpreter, so we only do it here so it only runs once
 
     while True:
         while True:
             try:
-                input_text = input_queue.get_nowait() #why can't I just use get()? dunno, but it hangs without nowait
+                #time.sleep(0.25)
+                if not input_queue.empty():
+                    print("found input at time", time.time())
+                input_text = input_queue.get(timeout=1) #why can't I just use get()? dunno, but it hangs without nowait
+                print('wtf')
                 break
             except queue.Empty:
                 pass
@@ -44,7 +50,7 @@ def worker_process(input_queue: queue.Queue[str], result_queue: queue.Queue[Tupl
                     mode = k
                     data = buffer[mode] + '** **'  # hack to preserve trailing \n in discord. doubles newline, though? why
 
-                    result_queue.put((mode, data))
+                    switch.get(mode, lambda data: print("ERROR: could not parse", data))(data)
                     buffer = {}
             else:
                 if len(buffer) != 1:
@@ -53,38 +59,25 @@ def worker_process(input_queue: queue.Queue[str], result_queue: queue.Queue[Tupl
                 message = list(buffer.items())[0]
                 mode, data = message
 
-                result_queue.put((mode, data))
+                switch.get(mode, lambda data: print("ERROR: could not parse", data))(data)
                 buffer = {k: v}
 
         if buffer:
             message = list(buffer.items())[0]
             mode, data = message
-            result_queue.put((mode, data))
-
-        result_queue.put(("end_of_response", ""))
-
-def result_listener(result_queue: queue.Queue[Tuple[str, str]]) -> None:
-    while True:
-        try:
-            mode, data = result_queue.get_nowait()
             switch.get(mode, lambda data: print("ERROR: could not parse", data))(data)
-        except queue.Empty:
-            pass
-        
+
+        print('%%END_OF_RESPONSE%%')
 
 def main() -> None:
     print('Welcome to unpaid intern! Send "reset" to reset the conversation.')
     print('%%END_OF_RESPONSE%%')
 
-    result_queue: multiprocessing.Queue[Tuple[str, str]] = multiprocessing.Queue()
     input_queue: multiprocessing.Queue[str] = multiprocessing.Queue()
 
     # Start the worker process for the first time
-    current_process = multiprocessing.Process(target=worker_process, args=(input_queue, result_queue))
+    current_process = multiprocessing.Process(target=worker_process, args=(input_queue,))
     current_process.start()
-
-    # Start the process listening to results
-    threading.Thread(target=result_listener, args=(result_queue,), daemon=True).start()
 
     while True:
         input_text = input()
@@ -94,7 +87,7 @@ def main() -> None:
                 current_process.join()
             
             # Start a new process after termination
-            current_process = multiprocessing.Process(target=worker_process, args=(input_queue, result_queue))
+            current_process = multiprocessing.Process(target=worker_process, args=(input_queue,))
             current_process.start()
             
             print("that intern did not get a return offer :(")
